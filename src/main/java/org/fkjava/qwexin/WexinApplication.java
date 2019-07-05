060feb0673de3147a714cc335152bdaeb09ee601
Arrow <Arrow@DESKTOP-OL31FG4>
2019-07-05 10:30:56 +0800

    test

--- src/main/java/org/fkjava/qwexin/WexinApplication.java
+++ src/main/java/org/fkjava/qwexin/WexinApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WexinApplication {
+	//test
 
 	public static void main(String[] args) {
 		SpringApplication.run(WexinApplication.class, args);
