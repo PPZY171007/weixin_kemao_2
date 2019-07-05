77fefbae9646f97e06f9c4ee86610f7c347217f6
Arrow <Arrow@DESKTOP-OL31FG4>
2019-07-05 10:30:13 +0800

    test# WARNING: head commit changed in the meantime
    test

---
+++ src/main/java/org/fkjava/qwexin/WexinApplication.java
@@ -0,0 +1,13 @@
+package org.fkjava.qwexin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WexinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WexinApplication.class, args);
+	}
+
+}
